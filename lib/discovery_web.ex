ee07df47a62d5afe2af2142f6db9f30a42cdb543
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-08-30 10:35:34 +0200

    Add homepage

--- lib/discovery_web.ex
+++ lib/discovery_web.ex
@@ -17,6 +17,8 @@ defmodule TurmsWeb do
   those modules here.
   """
 
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+
   def router do
     quote do
       use Phoenix.Router, helpers: false
@@ -24,6 +26,7 @@ defmodule TurmsWeb do
       # Import common connection and controller functions to use in pipelines
       import Plug.Conn
       import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
@@ -42,6 +45,52 @@ defmodule TurmsWeb do
       import Plug.Conn
       import TurmsWeb.Gettext
 
+      unquote(verified_routes())
+    end
+  end
+
+  def live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {TurmsWeb.Layouts, :app}
+
+      unquote(html_helpers())
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+
+      unquote(html_helpers())
+    end
+  end
+
+  def html do
+    quote do
+      use Phoenix.Component
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_csrf_token: 0, view_module: 1, view_template: 1]
+
+      # Include general helpers for rendering HTML
+      unquote(html_helpers())
+    end
+  end
+
+  defp html_helpers do
+    quote do
+      # HTML escaping functionality
+      import Phoenix.HTML
+      # Core UI components and translation
+      import TurmsWeb.CoreComponents
+      import TurmsWeb.Gettext
+
+      # Shortcut for generating JS commands
+      alias Phoenix.LiveView.JS
+
+      # Routes generation with the ~p sigil
       unquote(verified_routes())
     end
   end
