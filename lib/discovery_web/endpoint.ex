9efd2f2696a3fdae1b7d0fc75fb083b5adbce8fd
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-09-11 15:25:29 +0200

    Add websocket (#3)
    * Add socket manager
    
    * Add discovery socket to endpoint

--- lib/discovery_web/endpoint.ex
+++ lib/discovery_web/endpoint.ex
@@ -15,6 +15,10 @@ defmodule TurmsWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
+  socket "/socket", TurmsWeb.DiscoverySocket,
+    websocket: true,
+    longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
