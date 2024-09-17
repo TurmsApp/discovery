41d1945242c9444437e22e05c4a4ff3edfb3b3da
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-09-17 19:51:34 +0200

    Fix column reference

--- priv/repo/migrations/20240916120241_create_messages.exs
+++ priv/repo/migrations/20240916120241_create_messages.exs
@@ -6,11 +6,11 @@ defmodule Turms.Repo.Migrations.CreateMessages do
       add :message, :string
       add :date, :utc_datetime
       add :attachements, {:array, :integer}
-      add :user_id, references(:users, on_delete: :nothing)
+      add :user_vanity, references(:users, column: :vanity, type: :string, on_delete: :nothing)
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:messages, [:user_id])
+    create index(:messages, [:user_vanity])
   end
 end
