fca2acaebf941aaf4a8be74d6ce65946a0d62848
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-10-06 13:48:44 +0200

    Working auth controller

--- lib/discovery_web/controllers/auth_controller.ex
+++ lib/discovery_web/controllers/auth_controller.ex
@@ -1,5 +1,7 @@
 defmodule TurmsWeb.AuthController do
   use TurmsWeb, :controller
+  alias TurmsWeb.Plugs.Response
+  alias TurmsWeb.Plugs.Authentification
 
   # API to allow users to connect or create account.
   def login_or_signup(conn, %{"vanity" => vanity} = params) do
@@ -7,15 +9,33 @@ defmodule TurmsWeb.AuthController do
 
     case user do
       nil ->
-        conn
-        |> put_status(:created)
-        |> json(%{message: "Hello, World!", status: "success"})
+        # Create new user into database.
+        attrs = %{
+          vanity: vanity,
+          name: vanity,
+          password: nil,
+          public: true
+        }
+
+        changeset = Turms.User.changeset(%Turms.User{}, attrs)
+        Turms.Repo.insert(changeset)
+
+        Response.success(conn, Authentification.generate(vanity), :created)
 
       user ->
         password = Map.get(params, "password", nil)
-        conn
-        |> put_status(:unauthorized)
-        |> json(%{message: "Hello, World!", status: "success", password: password})
+
+        case user.hashed_password do
+          nil ->
+            Response.success(conn, Authentification.generate(vanity))
+
+          hashed_password ->
+            if !password or !Argon2.verify_pass(password, hashed_password) do
+              Response.error(conn, "Invalid password.", :unauthorized)
+            else
+              Response.success(conn, Authentification.generate(vanity))
+            end
+        end
     end
   end
 end
