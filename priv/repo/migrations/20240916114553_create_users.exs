16fed02fd44ef3ffbd6ca1ed399e6527d93421a7
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-10-06 13:39:49 +0200

    Nullable `hashed_password` field

--- priv/repo/migrations/20240916114553_create_users.exs
+++ priv/repo/migrations/20240916114553_create_users.exs
@@ -5,11 +5,8 @@ defmodule Turms.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :vanity, :string, primary_key: true
       add :name, :string
-      add :password, :string
-      add :date, :utc_datetime
+      add :hashed_password, :string, default: nil, null: true
       add :public, :boolean, default: false, null: false
-
-      timestamps(type: :utc_datetime)
     end
 
     create unique_index(:users, [:vanity])
