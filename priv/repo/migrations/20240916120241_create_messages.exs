ea94c534e9d0b9150b4c9775bf11a227cc96ab97
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-11-29 18:40:16 +0100

    Update user schema

--- priv/repo/migrations/20240916120241_create_messages.exs
+++ priv/repo/migrations/20240916120241_create_messages.exs
@@ -1,14 +1,13 @@
-defmodule Turms.Repo.Migrations.CreateMessages do
+defmodule Turms.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:messages) do
-      add :content, :string
-      add :from, :string
-      add :user_vanity, references(:users, column: :vanity, type: :string)
+    create table(:users, primary_key: false) do
+      add :vanity, :string, primary_key: true
+      add :name, :string
+      add :hashed_password, :string, default: nil, null: true
+      add :public, :boolean, default: false, null: false
       timestamps()
     end
-
-  create index(:messages, [:user_vanity])
   end
 end
