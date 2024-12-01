94cee64118272041794190af2c966581b81ad18b
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-01 20:29:43 +0100

    Split functions
    Fix Credo warning.

--- lib/discovery_web/controllers/auth_controller.ex
+++ lib/discovery_web/controllers/auth_controller.ex
@@ -5,37 +5,48 @@ defmodule TurmsWeb.AuthController do
 
   # API to allow users to connect or create account.
   def login_or_signup(conn, %{"vanity" => vanity} = params) do
-    user = Turms.Repo.get_by(Turms.User, vanity: vanity)
-
-    case user do
+    case Turms.Repo.get_by(Turms.User, vanity: vanity) do
       nil ->
-        # Create new user into database.
-        attrs = %{
-          vanity: vanity,
-          name: vanity,
-          password: nil,
-          public: true
-        }
-
-        changeset = Turms.User.changeset(%Turms.User{}, attrs)
-        Turms.Repo.insert(changeset)
-
-        Response.success(conn, Authentification.generate(vanity), :created)
+        create_new_user(conn, vanity)
 
       user ->
-        password = Map.get(params, "password", nil)
+        handle_existing_user(conn, user, params)
+    end
+  end
 
-        case user.hashed_password do
-          nil ->
-            Response.success(conn, Authentification.generate(vanity))
+  defp create_new_user(conn, vanity) do
+    attrs = %{
+      vanity: vanity,
+      name: vanity,
+      password: nil,
+      public: true
+    }
 
-          hashed_password ->
-            if !password or !Argon2.verify_pass(password, hashed_password) do
-              Response.error(conn, "Invalid password.", :unauthorized)
-            else
-              Response.success(conn, Authentification.generate(vanity))
-            end
-        end
+    changeset = Turms.User.changeset(%Turms.User{}, attrs)
+    Turms.Repo.insert(changeset)
+
+    Response.success(conn, Authentification.generate(vanity), :created)
+  end
+
+  defp handle_existing_user(conn, user, %{"password" => password}) do
+    case user.hashed_password do
+      nil ->
+        Response.success(conn, Authentification.generate(user.vanity))
+
+      hashed_password ->
+        verify_password(conn, password, hashed_password, user.vanity)
+    end
+  end
+
+  defp handle_existing_user(conn, user, _params) do
+    Response.success(conn, Authentification.generate(user.vanity))
+  end
+
+  defp verify_password(conn, password, hashed_password, vanity) do
+    if !password or !Argon2.verify_pass(password, hashed_password) do
+      Response.error(conn, "Invalid password.", :unauthorized)
+    else
+      Response.success(conn, Authentification.generate(vanity))
     end
   end
 end
