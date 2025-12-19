765023828a73e03839473e18727105195aa06499
RealHinome <acn@gravitalia.com>
2025-12-19 23:27:39 +0100

    Fix some warnings

--- config/config.exs
+++ config/config.exs
@@ -39,7 +39,7 @@ config :esbuild,
   version: "0.25.4",
   discovery: [
     args:
-      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
+      ~w(--bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ]
