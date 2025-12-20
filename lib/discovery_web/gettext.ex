0980369320e2a88365336a9adb9ec902d16281eb
RealHinome <acn@gravitalia.com>
2025-12-20 17:37:20 +0100

    Fix warning

--- lib/discovery_web/gettext.ex
+++ lib/discovery_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule TurmsWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :discovery
+  use Gettext, backend: TurmsWeb.Gettext
 end
